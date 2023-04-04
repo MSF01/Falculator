52a7b79fb17f762aa7a9bdd325057766e3fd75d2
unknown <thatxxshayaan@proton.me>
2023-04-05 00:56:16 +0500

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:calculator/Pages/login.dart';
 import 'package:flutter/material.dart';
 import 'Pages/home.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Calculator());
@@ -11,9 +12,11 @@ class Calculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.purple),
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
