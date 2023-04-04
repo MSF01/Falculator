4af1a8f5a8455e44bc4b082cd0d8b9c9b7a0a0b0
unknown <thatxxshayaan@proton.me>
2023-04-04 23:52:28 +0500

    day 3 completed

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -3,18 +3,18 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   final String devName = "Shayaan";
 
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculator"),
+        title: const Text("Calculator"),
       ),
       body: Center(
-        child: Container(
-          child: Text("Scientific Calculator by $devName"),
-        ),
+        child: Text("Scientific Calculator by $devName"),
       ),
-      drawer: Drawer(),
+      drawer: const Drawer(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
+import 'package:calculator/Pages/login.dart';
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'Pages/home.dart';
 
 void main() {
   runApp(Calculator());
 }
 
 class Calculator extends StatelessWidget {
-  Calculator({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
